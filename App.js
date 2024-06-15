0767f18b9ec51fe9165ace243ea6019e386d4e9d
shivam-saway <shivamsaway123@gmail.com>
2024-06-15 11:01:45 +0530

    Episode 1  - Formated code

--- App.js
+++ App.js
@@ -2,14 +2,14 @@
  * heading - React Element
  */
 const heading = React.createElement(
-    "h1",
-    {
-        id: "heading", // here, we can give attributes to the element.
-        },
-        "Hello World from React - App.js!"
-        ); // React API to create html element for us.
-        
-        /** heading as of now is a javascript object. 
+  "h1",
+  {
+    id: "heading", // here, we can give attributes to the element.
+  },
+  "Hello World from React - App.js!"
+); // React API to create html element for us.
+
+/** heading as of now is a javascript object. 
          * At this stage the heading variable is a react element and not a DOM element.
          * It is the job of react dom to prepare the virtual DOM first and then update the actual DOM.
          * 
@@ -28,10 +28,10 @@ const heading = React.createElement(
         * As we can see here the heading object as property props.
         * The props is the key feature of react and it is the attribute and childrens of the parent object which we create using createElement API from React.
         * */
-       console.log(heading);
-       
-       /** ReactDOM API to tell react engine about the root container under which DOM manipulation will be done. */
-       const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+
+/** ReactDOM API to tell react engine about the root container under which DOM manipulation will be done. */
+const root = ReactDOM.createRoot(document.getElementById("root"));
 /** ReactDOM API to update the actual DOM in web browser. */
 root.render(heading);
 
