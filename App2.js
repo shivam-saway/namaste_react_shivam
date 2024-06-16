d22e71da3bf41ff13f9ed139ff575cf3082cf634
shivam-saway <shivamsaway123@gmail.com>
2024-06-16 15:06:03 +0530

    Episode 1 - Multiple children of the parent with react create element

--- App2.js
+++ App2.js
@@ -4,19 +4,32 @@
  *
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child-one" }, [
     React.createElement(
       "h1",
-      { id: "heading" },
+      { id: "child-one-heading", key: "child-one-heading" },
       "Hello World from React by Shivam."
-    )
-  )
-);
+    ),
+    React.createElement(
+      "h1",
+      { id: "child-one-heading-two", key: "child-one-heading-two" },
+      "Hello World two from React by Shivam."
+    ),
+  ]),
+  React.createElement("div", { id: "child-two" }, [
+    React.createElement(
+      "h1",
+      { id: "child-two-heading", key: "child-two-heading" },
+      "Hello World from React by Shivam."
+    ),
+    React.createElement(
+      "h1",
+      { id: "child-two-heading-two", key: "child-two-heading-two" },
+      "Hello World two from React by Shivam."
+    ),
+  ]),
+]);
 
 console.log(parent);
 
